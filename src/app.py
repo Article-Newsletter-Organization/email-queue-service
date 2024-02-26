d1f607548d9026cda38d25764b9d112def89db7d
emanuelvasconnobre <emanuelvanobre@gmail.com>
2024-02-26 15:21:29 -0300

    feat: database app integration

--- src/app.py
+++ src/app.py
@@ -1,13 +1,14 @@
 from log.custom_logger import CustomLogger
+from data.db.publication.repository import PublicationRepository
+from data.db.subscribers.repository import SubscriberRepository
 
 
 class App():
     logger = CustomLogger()
 
     def __init__(self):
-        """
-            Constructor
-        """
+        self.subscriber_repo = SubscriberRepository()
+        self.publication_repo = PublicationRepository()
 
     def run(self):
         self.logger.info("Application running!")
