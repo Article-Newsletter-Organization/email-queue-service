aa1eceb13137ae198d8d5129006cadd784e78b81
emanuelvasconnobre <emanuelvanobre@gmail.com>
2024-02-26 15:20:14 -0300

    refac: correcting "connexion_error"

--- src/errors/database/connection_error.py
+++ src/errors/database/connection_error.py
@@ -1,7 +1,7 @@
 from errors.custom_error import CustomError
 
 
-class ConnexionError(CustomError):
+class ConnectionError(CustomError):
     def __init__(self, message=None, traceback=None, issues=None):
         super().__init__(
             message or "Ocorreu um erro ao tentar se conectar ao banco de dados!",
