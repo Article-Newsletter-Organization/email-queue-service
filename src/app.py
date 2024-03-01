ca57c899ade7d9e2c38feff1188582b9f6fc45f8
emanuelvasconnobre <emanuelvanobre@gmail.com>
2024-03-01 11:18:52 -0300

    feat: app builder created

--- src/app.py
+++ src/app.py
@@ -1,14 +1,30 @@
 from log.custom_logger import CustomLogger
-from data.db.publication.repository import PublicationRepository
-from data.db.subscribers.repository import SubscriberRepository
+from infra.postgres.sqlalchemy_connect import initialize as sqlalchemy_init
+from domain.services.database_service import DatabaseService
+from errors.custom_error import CustomError
+from errors.unexpected_error import UnexpectedError
 
 
-class App():
-    logger = CustomLogger()
+class App:
+    logger = CustomLogger("Application")
+    database_service = DatabaseService()
 
     def __init__(self):
-        self.subscriber_repo = SubscriberRepository()
-        self.publication_repo = PublicationRepository()
+        sqlalchemy_init()
+
+    def queueCallback(self):
+        subscribers = self.database_service.getSubscribers()
+
+    def exceptionHandler(self, error: CustomError):
+        self.logger.error(error.message, error, metadata=error.metadata)
 
     def run(self):
-        self.logger.info("Application running!")
+        try:
+            self.logger.info("Application running!")
+            self.queueCallback()
+        except CustomError as error:
+            self.exceptionHandler(error)
+        except Exception as e:
+            custom_error = UnexpectedError(traceback=str(e))
+
+            self.logger.error(custom_error.message, custom_error)
