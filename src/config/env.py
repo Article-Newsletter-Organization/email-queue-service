76fad55c1a633fd5d80aaedbebd8d799f39b68c0
emanuelvasconnobre <emanuelvanobre@gmail.com>
2024-04-04 16:47:54 -0300

    refac: env.py reconfigurated

--- src/config/env.py
+++ src/config/env.py
@@ -1,11 +1,18 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
 
 def getEnvParams() -> dict:
-    load_dotenv()
+    load_dotenv(dotenv_path=find_dotenv(), override=True)
 
     return {
+        "smtp": {
+            "username": os.getenv("SMTP_USERNAME"),
+            "password": os.getenv("SMTP_PASSWORD"),
+            "email": os.getenv("SMTP_EMAIL"),
+            "server": os.getenv("SMTP_SERVER"),
+            "port": os.getenv("SMTP_PORT"),
+        },
         "postgres": {
             "database_url": os.getenv("POSTGRES_DATABASE_URL"),
         },
@@ -14,7 +21,7 @@ def getEnvParams() -> dict:
             "port": os.getenv("RABBIT_PORT"),
             "username": os.getenv("RABBIT_USERNAME"),
             "password": os.getenv("RABBIT_PASSWORD"),
-            "queue_name": os.getenv("RABBIT_QUEUE_NAME"),
+            "topic_name": os.getenv("RABBIT_TOPIC_NAME"),
         },
         "app": {},
     }
